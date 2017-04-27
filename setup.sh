faf9888a8d618b26da50ea34a573c2cc51507a22
Chris Kenyon <cjkenyon117@gmail.com>
2017-04-27 00:43:13 -0400

    Updated the setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,25 @@
 ######## Apt-get Installs ########
-apt-get update
-apt-get install vim
-apt-get install tmux
-apt-get install python3
+sudo apt-get update
+sudo apt-get -y upgrade
+# Basics
+sudo apt-get -y install vim
+sudo apt-get -y install tmux
+sudo apt-get -y install chromium-browser
+# Python
+sudo apt-get -y install python3
+sudo apt-get -y install build-essential
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install libffi-dev
+sudo apt-get -y install python3-dev
+sudo apt-get -y install python3-pip
 
 ######## Python Installs #########
-pip3 install numpy
-pip3 install scipy
-pip3 install
+sudo pip3 install --upgrade pip
+sudo pip3 install numpy
+sudo pip3 install scipy
+sudo pip3 install requests
+sudo pip3 install sqlalchemy
+sudo pip3 install matplotlib
 
 ######## Configs #############
 cp .vimrc ~/.vimrc
@@ -17,5 +29,5 @@ cp .tmux.conf ~/.tmux.conf
 ######## Open source installs #######
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Please open tmux and then PREFIX+I"
